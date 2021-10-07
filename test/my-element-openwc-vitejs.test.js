0c8e396696ece76142d932ccba57237bd113b83f
Oscar marina <oscar7600@outlook.com>
2021-10-07 11:54:49 +0200

    feat: add test - error and skip

--- test/my-element-openwc-vitejs.test.js
+++ test/my-element-openwc-vitejs.test.js
@@ -40,3 +40,33 @@ describe('MyElementOpenwcVitejs', () => {
     await expect(el).shadowDom.to.be.accessible();
   });
 });
+
+describe('MyElementOpenwcVitejs DUPLICATE Fail - Skip', () => {
+  it.skip('has a default heading "Hey there" and counter 5', async () => {
+    const el = await fixture(
+      html`<my-element-openwc-vitejs></my-element-openwc-vitejs>`
+    );
+
+    expect(el.heading).to.equal('Hey there');
+    expect(el.counter).to.equal(5);
+  });
+
+  it('increases the counter on button click', async () => {
+    const el = await fixture(
+      html`<my-element-openwc-vitejs></my-element-openwc-vitejs>`
+    );
+    el.shadowRoot.querySelector('button').click();
+
+    expect(el.counter).to.equal(16);
+  });
+
+  it('can override the heading via attribute', async () => {
+    const el = await fixture(
+      html`<my-element-openwc-vitejs
+        heading="attribute heading"
+      ></my-element-openwc-vitejs>`
+    );
+
+    expect(el.heading).to.equal('attribute heading');
+  });
+});
